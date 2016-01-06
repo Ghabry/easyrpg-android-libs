75ee3b8e5ecb8d91274249699d24a16fe6bcc39b
BlisterBoy <mehdi.abelha@gmail.com>
2016-01-06 13:36:50 +0100

    Different installation depending on the OS (darwin + linux)

--- 1_download_library.sh
+++ 1_download_library.sh
@@ -1,13 +1,33 @@
+#!/bin/bash
+
 #Verify to have "hg" command
 #Verify the autoconf command
 
-#Compile toolchain
+# Supported os : "darwin" ou "linux"
+os=$1
+darwin="darwin"
+linux="linux"
 
+if [ $os = $darwin ] ; then
+	echo "Darwin detected"
+elif [ $os = $linux ] ; then
+	echo "Linux detected"
+else
+	echo "OS not detected"
+	exit
+fi
+
+exit
+
+#Compile toolchain
 #Android SDK
 #Linux
-wget http://dl.google.com/android/android-sdk_r24.4.1-linux.tgz
+if [ $os = $linux ] ; then
+	wget http://dl.google.com/android/android-sdk_r24.4.1-linux.tgz
 #Mac
-wget http://dl.google.com/android/android-sdk_r24.4.1-macosx.zip
+elif [ $os = $darwin ] ; then
+	wget http://dl.google.com/android/android-sdk_r24.4.1-macosx.zip
+fi
 
 # Install SDK12 and Platform-tools
 num=$(android list sdk --all | grep "Android SDK Build-tools, revision 23.0.2" | head -n1 | awk '{gsub(/[^0-9]/,"", $1);print $1}')
@@ -22,16 +42,18 @@ echo "y" | android update sdk -u -a -t $num
 
 #Android NDK : depend on plateform
 #Linux
-wget http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin
-chmod u+x android-ndk-r10e-linux-x86_64.bin
-./android-ndk-r10e-linux-x86_64.bin
-rm android-ndk-r10e-linux-x86_64.bin
+if [ $os = $linux ] ; then
+	wget http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin
+	chmod u+x android-ndk-r10e-linux-x86_64.bin
+	./android-ndk-r10e-linux-x86_64.bin
+	rm android-ndk-r10e-linux-x86_64.bin
 #Mac
-wget http://dl.google.com/android/ndk/android-ndk-r10e-darwin-x86_64.bin
-chmod u+x android-ndk-r10e-darwin-x86_64.bin
-./android-ndk-r10e-darwin-x86_64.bin
-rm android-ndk-r10e-darwin-x86_64.bin
-
+elif [ $os = $darwin ] ; then
+	wget http://dl.google.com/android/ndk/android-ndk-r10e-darwin-x86_64.bin
+	chmod u+x android-ndk-r10e-darwin-x86_64.bin
+	./android-ndk-r10e-darwin-x86_64.bin
+	rm android-ndk-r10e-darwin-x86_64.bin
+fi
 
 wget http://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.bz2
 wget http://prdownloads.sourceforge.net/libpng/libpng-1.6.20.tar.xz
@@ -41,8 +63,6 @@ wget http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.xz
 wget http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.xz
 wget ftp://ftp.mars.org/pub/mpeg/libmad-0.15.1b.tar.gz
 wget http://sourceforge.net/projects/modplug-xmms/files/libmodplug/0.8.8.5/libmodplug-0.8.8.5.tar.gz
-
-
 hg clone http://hg.libsdl.org/SDL
 hg clone http://hg.libsdl.org/SDL_mixer
 wget http://download.icu-project.org/files/icu4c/56.1/icu4c-56_1-src.tgz
