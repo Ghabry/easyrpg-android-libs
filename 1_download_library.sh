011a10dc1e75700206d0fa478a8eb48b6e1fcb81
BlisterBoy <mehdi.abelha@gmail.com>
2016-01-07 13:29:18 +0100

    Continuation

--- 1_download_library.sh
+++ 1_download_library.sh
@@ -17,15 +17,23 @@ else
 	exit
 fi
 
+export WORKSPACE=$PWD
+
 #Compile toolchain
 #Android SDK
 #Linux
 if [ $os = $linux ] ; then
 	wget http://dl.google.com/android/android-sdk_r24.4.1-linux.tgz
+	tar xf android-sdk_r24.4.1-linux.tgz
+	mv android-sdk-linux android-sdk
 #Mac
 elif [ $os = $darwin ] ; then
 	wget http://dl.google.com/android/android-sdk_r24.4.1-macosx.zip
+	tar xf android-sdk_r24.4.1-macosx.zip
+	mv android-sdk-macosx android-sdk
 fi
+
+PATH=$PATH:$WORKSPACE/android-ndk-r10e:$WORKSPACE/android-sdk/tools
 
 # Install SDK12 and Platform-tools
 num=$(android list sdk --all | grep "Android SDK Build-tools, revision 23.0.2" | head -n1 | awk '{gsub(/[^0-9]/,"", $1);print $1}')
@@ -44,13 +52,13 @@ if [ $os = $linux ] ; then
 	wget http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin
 	chmod u+x android-ndk-r10e-linux-x86_64.bin
 	./android-ndk-r10e-linux-x86_64.bin
-	rm android-ndk-r10e-linux-x86_64.bin
+	#rm android-ndk-r10e-linux-x86_64.bin
 #Mac
 elif [ $os = $darwin ] ; then
 	wget http://dl.google.com/android/ndk/android-ndk-r10e-darwin-x86_64.bin
 	chmod u+x android-ndk-r10e-darwin-x86_64.bin
 	./android-ndk-r10e-darwin-x86_64.bin
-	rm android-ndk-r10e-darwin-x86_64.bin
+	#rm android-ndk-r10e-darwin-x86_64.bin
 fi
 
 # Install boost header
@@ -69,10 +77,28 @@ tar xf freetype-2.6.tar.bz2
 tar xf pixman-0.32.8.tar.gz
 wget http://cairographics.org/releases/pixman-0.32.8.tar.gz
 
+# Install libogg
 wget http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.xz
+tar xf libogg-1.3.2.tar.xz
+
+# Install libvorbis
 wget http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.xz
+tar xf libvorbis-1.3.5.tar.xz
+
+# Install libmad
 wget ftp://ftp.mars.org/pub/mpeg/libmad-0.15.1b.tar.gz
+tar xf libmad-0.15.1b.tar.gz
+
+# Install libmodplug
 wget http://sourceforge.net/projects/modplug-xmms/files/libmodplug/0.8.8.5/libmodplug-0.8.8.5.tar.gz
+tar xf libmodplug-0.8.8.5.tar.gz
+
+# Install SDL2
 hg clone http://hg.libsdl.org/SDL
+
+# Install SDL2_mixer
 hg clone http://hg.libsdl.org/SDL_mixer
+
+# Install ICU
 wget http://download.icu-project.org/files/icu4c/56.1/icu4c-56_1-src.tgz
+tar xf icu4c-56_1-src.tgz
